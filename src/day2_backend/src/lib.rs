9ffd463ef6632b805ad34d3896f762a664c221dc
Wawrzyniec <wawrzyniec.korzen@gmail.com>
2024-06-25 19:24:41 +0200

    Blog added and some backend functions

--- src/day2_backend/src/lib.rs
+++ src/day2_backend/src/lib.rs
@@ -1,4 +1,24 @@
+use std::cell::RefCell;
+
+thread_local! 
+{
+    static WPISY: RefCell<Vec<String>> = RefCell::default();
+}
+
+
 #[ic_cdk::query]
 fn greet(name: String, number: i8 ) -> String {
     format!("Hello, {} {}!", name, number)
+}
+
+#[ic_cdk::update]
+fn dodaj_wpis(wpis: String)
+{
+    WPISY.with(|wpisy|{wpisy.borrow_mut().push(wpis)});
+}
+
+#[ic_cdk::query]
+fn odczyta_wpisy() -> Vec<String>
+{
+    WPISY.with(|wpisy|{wpisy.borrow().clone()})
 }
