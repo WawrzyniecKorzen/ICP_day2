d18c54f036afe08e672b8cb4084d729feeb1089e
Wawrzyniec <wawrzyniec.korzen@gmail.com>
2024-06-26 19:16:55 +0200

    troche polerowanka

--- src/day2_frontend/src/components/Blog.vue
+++ src/day2_frontend/src/components/Blog.vue
@@ -1,14 +1,21 @@
 <template>
     <div>
         <h2 class="text-blue-600">Wpisy na bloga</h2>
-        <button @click="pobierzWpisy">Pobierz wpisy</button>
-        <div v-for="wpis in wpisy">
-            <p>{{wpis}}</p>
+        <div class="w-100 flex flex-row-reverse">
+            <button @click="pobierzWpisy" class="bg-blue-600 rounded text-white p-4">Pobierz wpisy</button>
         </div>
+        <div class="grid mx-6 gap-4 my-4">
 
-        <input v-model="nowyBlog" type="text">
-        <button @click="dodajWpis">Dodaj wpis</button>
-        {{ nowyBlog }}
+            <div v-for="wpis in wpisy" class="drop-shadow-xl bg-stone-300 p-4">
+                <p>{{wpis}}</p>
+            </div>
+        </div>
+        
+        <div class="flex justify-center flex-col mx-6 gap-4 my-4">
+            <input v-model="nowyBlog" class="border-2 border-blue-600 p-4 w-" type="text" >
+            <button @click="dodajWpis" class="bg-blue-600 rounded text-white p-4">Dodaj wpis</button>
+        </div>
+        
     </div>
 </template>
 
